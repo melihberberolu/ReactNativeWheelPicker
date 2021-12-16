9ef8c7853095de6a3c80aa80f300d1c5c6872d77
Victor Zimnikov <victor.zimnikov@yandex.ru>
2021-12-16 17:47:00 +0200

    Updated

--- index.tsx
+++ index.tsx
@@ -1,5 +1,8 @@
+// @ts-ignore
 import WheelPicker from './src/WheelPicker'
+// @ts-ignore
 import TimePicker from './src/TimePicker'
+// @ts-ignore
 import DatePicker from './src/DatePicker'
 
 export { WheelPicker, TimePicker, DatePicker }
