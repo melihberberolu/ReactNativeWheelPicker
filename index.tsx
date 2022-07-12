f15bd3099d344efb99f2e7fc57a68b95b069ec6e
Victor Zimnikov <victor.zimnikov@yandex.ru>
2022-07-12 12:29:09 +0200

    Добавлено: Экспорт Picker, PickerIOS, PickerItemProps, ItemValue

--- index.tsx
+++ index.tsx
@@ -5,4 +5,36 @@ import TimePicker from './src/TimePicker'
 // @ts-ignore
 import DatePicker from './src/DatePicker'
 
-export { WheelPicker, TimePicker, DatePicker }
+import { Picker, PickerIOS } from "@react-native-picker/picker";
+
+import { StyleProp, TextStyle } from "react-native";
+
+type ItemValue = number | string;
+
+interface PickerItemProps<T = ItemValue> {
+  label?: string;
+  value?: T;
+  color?: string;
+  fontFamily?: string;
+  testID?: string;
+  /**
+   * Style to apply to individual item labels.
+   * Only following values take effect:
+   *   - 'color'
+   *   - 'backgroundColor'
+   *   - 'fontSize'
+   *   - 'fontFamily'
+   *
+   * @platform android
+   */
+  style?: StyleProp<TextStyle>;
+  /**
+   * If set to false, the specific item will be disabled, i.e. the user will not be able to make a
+   * selection.
+   * @default true
+   * @platform android | web
+   */
+  enabled?: boolean;
+}
+
+export { WheelPicker, TimePicker, DatePicker, Picker, PickerIOS, PickerItemProps, ItemValue }
